317a6a1a0342afadc12ac2689b654ae4090d89cd
anusha414 <53256022+anusha414@users.noreply.github.com>
2026-08-26 14:58:56 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import mlflow
+import mlflow.sklearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import joblib
+
+# Configure MLflow
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("experiment1")
 
 # Load data
 df = pd.read_csv("student_scores.csv")
@@ -16,18 +22,32 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train model
-model = LogisticRegression()
-model.fit(X_train, y_train)
+# Start MLflow run
+with mlflow.start_run():
 
-# Predictions
-predictions = model.predict(X_test)
+    # Log parameters
+    mlflow.log_param("model_type", "LogisticRegression")
+    mlflow.log_param("test_size", 0.2)
+    mlflow.log_param("random_state", 42)
 
-# Accuracy
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy:", accuracy)
+    # Train model
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
 
-# Save model
-joblib.dump(model, "student_pass_model.pkl")
+    # Predictions
+    predictions = model.predict(X_test)
 
-print("Model saved successfully")
+    # Accuracy
+    accuracy = accuracy_score(y_test, predictions)
+    print("Accuracy:", accuracy)
+
+    # Log metric
+    mlflow.log_metric("accuracy", accuracy)
+
+    # Save model
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+
+    print("Model logged to MLflow successfully")​‌
